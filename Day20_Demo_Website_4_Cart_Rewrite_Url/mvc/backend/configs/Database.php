2842e558d4ecc309a02a40d48e11275a4e5142f8
Nguyen Viet Manh <nguyenvietmanhit@gmail.com>
2022-06-24 12:14:02 +0700

    update

--- Day20_Demo_Website_4_Cart_Rewrite_Url/mvc/backend/configs/Database.php
+++
@@ -1,6 +0,0 @@
-<?php
-class Database {
-  const DB_DSN = 'mysql:host=localhost;dbname=mvc_demo;charset=utf8';
-  const DB_USERNAME = 'root';
-  const DB_PASSWORD = '';
-}
--- Day20_Demo_Website_4_Cart_Rewrite_Url/mvc/frontend/controllers/CartController.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once 'controllers/Controller.php';
-require_once 'models/Product.php';
-
-class CartController extends Controller
-{
-
-}
